68dd68c1d9c7c389a609c950ff77648b6dc0bcf0
ontajoka <onni.krjn@gmail.com>
2022-10-19 20:44:34 +0300

    Lisätty yksi uusi ikkuna treenin muokkausta varten sekä treeni.jar -tiedosto

--- src/Treenisovellus/TreenisovellusGUIController.java
+++ src/Treenisovellus/TreenisovellusGUIController.java
@@ -1,10 +1,29 @@
 package Treenisovellus;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.fxml.FXML;
+
 /**
  * @author Onni
  * @version 7.10.2022
  *
  */
 public class TreenisovellusGUIController {
-      // TODO
+
+    @FXML void handleUusiLiike() {
+        Dialogs.showMessageDialog("Vielä ei osata lisätä treeniliikkeitä");
+    }
+
+    @FXML void handleUusiSuoritus() {
+        Dialogs.showMessageDialog("Vielä ei osata lisätä suorituksia");
+    }
+    
+    @FXML void handleVersio() {
+        Dialogs.showMessageDialog("Vielä ei osata katsoa versiota");
+    }
+    
+    @FXML void handleMuokkaaSuoritusta() {
+        Dialogs.showMessageDialog("Ei pysty muokkaamaan aiempia suorituksia vielä");
+    }
+
 }
