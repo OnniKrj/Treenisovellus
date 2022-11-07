b253841d9285b166fe47c776f1c8fb8152554827
ontajoka <onni.krjn@gmail.com>
2022-11-07 19:47:01 +0200

    Suoritus -luokkaan tehty tulosta, kirjaa ja getTreeniNro -metodit

--- src/treeni/Suoritus.java
+++ src/treeni/Suoritus.java
@@ -12,11 +12,60 @@ import java.io.PrintStream;
  */
 public class Suoritus {
     
+    private int treeniNro;
+    private int liikeNro = 0;
+    private int sarjaMaara = 0;
+    private int toistoMaara = 0;
+    private double paino = 0;
+    private String pvm = "";
+    
+    private static int seuraavaNro = 1;
+    
+    /**
+     * Alustetaan suoritus tyhj‰ksi
+     */
+    public Suoritus() {
+        //
+    }
+    
     /**
      * @param out Tulostettava tietovirta
      */
     public void tulosta(PrintStream out) {
-        out.println("Treeni1");
+        out.println(String.format("%03d", treeniNro));
+        out.println("Liikkeen tunnus: " + liikeNro);
+        out.println("Sarjojen m‰‰r‰: " + sarjaMaara);
+        out.println("Toistojen m‰‰r‰: " + toistoMaara);
+        out.println("Sarja paino: " + paino);
+        out.println("P‰iv‰m‰‰r‰: " + pvm + "\n");
+    }
+    
+    /**
+     * @return Treenin id-numero
+     * @example
+     * <pre name="test">
+     * Suoritus treeni1 = new Suoritus();
+     * treeni1.getTreeniNro() === 0;
+     * treeni1.kirjaa();
+     * Suoritus treeni2 = new Suoritus();
+     * treeni2.kirjaa();
+     * int tunnus1 = treeni1.getTreeniNro();
+     * int tunnus2 = treeni2.getTreeniNro();
+     * tunnus2 === tunnus1 + 1;
+     * </pre>
+     */
+    public int kirjaa() {
+        this.treeniNro = seuraavaNro;
+        seuraavaNro++;
+        return this.treeniNro;
+    }
+    
+    
+    /**
+     * @return Treenin treeni-id
+     */
+    public int getTreeniNro() {
+        return treeniNro;
     }
 
     /**
@@ -26,8 +75,8 @@ public class Suoritus {
         Suoritus treeni1 = new Suoritus();
         Suoritus treeni2 = new Suoritus();
         
-        //treeni1.kirjaa();
-        //treeni2.kirjaa();
+        treeni1.kirjaa();
+        treeni2.kirjaa();
         
         treeni1.tulosta(System.out);
         treeni2.tulosta(System.out);
